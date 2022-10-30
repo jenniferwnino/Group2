7e5742326965821666db8052cf5e566957e33343
jenniferwnino <80395791+jenniferwnino@users.noreply.github.com>
2022-10-30 14:50:07 -0400

    Updates to main and game 1

--- TLTQ/src/main.cpp
+++ TLTQ/src/main.cpp
@@ -1,23 +1,57 @@
 #include <SFML/Graphics.hpp>
+#include "Game1.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    int windowStatus = 0;
+
+    sf::RenderWindow window(sf::VideoMode(480, 270), "Climate Stompers");
+    sf::Vector2i position;
+
+    sf::Texture mainTexture;
+    mainTexture.loadFromFile("graphics/climateStompersMainMenu.png");
+    sf::Sprite mainSprite(mainTexture);
+
+    Game1 game1;
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // Check if the window was closed by clicking "X"
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Check if window was closed by hitting "esc" key
+            if (event.type == sf::Event::KeyPressed) {
+                if(event.key.code == sf::Keyboard::Escape)
+                    window.close();
+
+            }
+
+            // TEMP SOLUTION TO MOVE TO GAME 1
+            if (event.type == sf::Event::KeyPressed) {
+                windowStatus = 1;
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                position = sf::Mouse::getPosition(window);
+            }
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        // Display main menu
+        if (windowStatus == 0) {
+            window.clear();
+            window.draw(mainSprite);
+            window.display();
+        }
+
+        // Run game 1
+        if (windowStatus == 1) {
+            game1.update(window, position);
+            game1.draw(window);
+        }
     }
 
     return 0;
