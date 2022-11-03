3628d08f8cba5fc589e6639945e63c271ff65b42
Chris Smiley <csmiley@ufl.edu>
2022-11-03 11:16:27 -0400

    Revert

--- TLTQ/src/Game1.h
+++ TLTQ/src/Game1.h
@@ -1,58 +1,36 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <vector>
 
 class Game1 {
+private:
+    int questionNumber;
+    bool questionResponse;
+    bool leftAnswer;
+    bool rightAnswer;
+    bool next;
+
+    sf::Event event;
+
+    sf::Texture game1Texture;
+    sf::Sprite game1Sprite;
+
+    sf::Font font;
+    sf::Text question1;
+    sf::Text correctText1;
+    sf::Text incorrectText1;
+    //sf::Text question2;
+    sf::Text correctText2;
+    sf::Text incorrectText2;
+
+    sf::Vector2i m_position;
+
 public:
-    // For individual question characteristics
-    struct Game1Question {
-    public:
-        sf::Text correctText;
-        sf::Text incorrectText;
-        bool answered;
-        bool answeredCorrectly;
-        bool leftCorrect;
-
-        Game1Question(std::string& correct, std::string& incorrect, bool left, sf::Font& font, int& fontSize, sf::Vector2f& pos);
-    };
-
     // Constructor
     Game1();
 
     // Public Functions
     void update(sf::RenderWindow &window, sf::Vector2i position);
     void draw(sf::RenderWindow &window);
-
-
-private:
-    int numCorrect;
-    int questionNumber;
-    float winCondition;
-
-    bool leftAnswer;
-    bool rightAnswer;
-    bool next;
-
-    int questionFontSize;
-    int answerFontSize;
-    sf::Vector2f questionPos;
-    sf::Vector2f answerPos;
-    sf::Font font;
-    sf::Text question;
-
-    std::string correctTemp;
-    std::string incorrectTemp;
-
-    sf::Texture game1Texture;
-    sf::Sprite game1Sprite;
-    sf::Texture game1WinTexture;
-    sf::Sprite game1WinSprite;
-    sf::Texture game1LoseTexture;
-    sf::Sprite game1LoseSprite;
-
-    std::vector<Game1Question> questions;
-
-    // Private functions
-    void correctResponse(sf::RenderWindow &window, Game1Question& question, int& numCorrect);
-    void incorrectResponse(sf::RenderWindow &window, Game1Question& question);
 };
+
+
